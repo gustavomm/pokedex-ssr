e4671e1a79da8f6aa4e021e7cbcce0ccf105afe5
Gustavo Mendes <gustavomm@Gustavos-MacBook-Pro.local>
2022-09-25 19:56:10 -0300

    fixed layout on details page on mobile

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -38,7 +38,7 @@ const PokemonDetails: NextPage<PokemonDetailsProps> = ({ pokemon }) => {
               </div>
             ))}
           </div>
-          <div className="grid cols-3 md:grid-cols-3 gap-8 p-5 dark:bg-slate-800 bg-slate-200 transition-color duration-500 rounded-lg mb-6">
+          <div className="grid cols-3 md:grid-cols-3 gap-3 md:gap-8 p-5 dark:bg-slate-800 bg-slate-200 transition-color duration-500 rounded-lg mb-6">
             <div className="flex flex-col">
               <span className="text-slate-500">Heigth</span>
               <span className="text-xl font-light">{`${
